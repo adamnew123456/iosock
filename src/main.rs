c8340cea883766c0f29c3274e0c7f50e23909b75
adamnew123456 <adamnew123456@gmail.com>
2025-12-29 00:18:13 -0500

    Code cleanup: commenting and formatting

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use std::cell::Cell;
 use std::env::args;
 use std::fmt::Display;
 use std::fs::remove_file;
-use std::io::{self, PipeReader, Read, Write, pipe, stdout};
+use std::io::{self, pipe, stdout, PipeReader, Read, Write};
 use std::os::fd::AsRawFd;
 use std::os::unix::net::{UnixListener, UnixStream};
-use std::process::{ChildStderr, ChildStdin, ChildStdout, Command, Stdio, exit};
+use std::process::{exit, ChildStderr, ChildStdin, ChildStdout, Command, Stdio};
 use std::thread::spawn;
 
 use epoll::{ControlOptions, Event, Events};
@@ -42,7 +42,7 @@ impl<T: Default + Copy, const N: usize> FlipBuffer<T, N> {
             write_page: Cell::new(empty_write.into_boxed_slice()),
             read_cursor: 0,
             read_max: 0,
-            write_cursor: 0
+            write_cursor: 0,
         }
     }
 
@@ -66,7 +66,10 @@ impl<T: Default + Copy, const N: usize> FlipBuffer<T, N> {
     /// `func` should return the number of bytes that were consumed from what
     /// was available. If `func` returns a value larger than the size of the
     /// slice, this function panics.
-    pub fn with_read<E, F: FnOnce(&[T]) -> Result<usize, E>>(&mut self, func: F) -> Result<usize, E> {
+    pub fn with_read<E, F: FnOnce(&[T]) -> Result<usize, E>>(
+        &mut self,
+        func: F,
+    ) -> Result<usize, E> {
         if self.read_cursor == self.read_max && self.write_cursor > 0 {
             // Steal the write buffer if it has some data we can read.
             self.flip_buffers();
@@ -77,12 +80,15 @@ impl<T: Default + Copy, const N: usize> FlipBuffer<T, N> {
         match func(slice) {
             Ok(was_read) => {
                 if was_read > readable {
-                    panic!("Readable range is {}..{} ({} bytes) but {} was consumed", self.read_cursor, self.read_max, readable, was_read);
+                    panic!(
+                        "Readable range is {}..{} ({} bytes) but {} was consumed",
+                        self.read_cursor, self.read_max, readable, was_read
+                    );
                 }
                 self.read_cursor += was_read;
                 Ok(was_read)
-            },
-            Err(err) => Err(err)
+            }
+            Err(err) => Err(err),
         }
     }
 
@@ -92,8 +98,11 @@ impl<T: Default + Copy, const N: usize> FlipBuffer<T, N> {
     /// `func` should return the number of bytes that were written into the
     /// provided slice. If `func` returns a value larger than the size of that
     /// slice, this function panics.
-    pub fn with_write<E, F: FnOnce(&mut [T]) -> Result<usize, E>>(&mut self, func: F) -> Result<usize, E> {
-        if self.write_cursor == N  && self.read_cursor == self.read_max {
+    pub fn with_write<E, F: FnOnce(&mut [T]) -> Result<usize, E>>(
+        &mut self,
+        func: F,
+    ) -> Result<usize, E> {
+        if self.write_cursor == N && self.read_cursor == self.read_max {
             // Steal the read buffer if there is no data that we would clobber.
             self.flip_buffers();
         }
@@ -103,12 +112,15 @@ impl<T: Default + Copy, const N: usize> FlipBuffer<T, N> {
         match func(slice) {
             Ok(was_written) => {
                 if was_written > writable {
-                    panic!("Writable range is {}..{} ({} bytes) but {} was written", self.write_cursor, N, writable, was_written);
+                    panic!(
+                        "Writable range is {}..{} ({} bytes) but {} was written",
+                        self.write_cursor, N, writable, was_written
+                    );
                 }
                 self.write_cursor += was_written;
                 Ok(was_written)
-            },
-            Err(err) => Err(err)
+            }
+            Err(err) => Err(err),
         }
     }
 }
@@ -176,7 +188,7 @@ fn socket_stream_bridge(
         for i in 0..events {
             let target = events_buf[i].data as i32;
             if target == notify_fd {
-                break 'events
+                break 'events;
             } else if target == child_stdout_fd {
                 child_stdouterr_buf.with_write(|buf| child_stdout.read(buf))?;
             } else if target == child_stderr_fd {
@@ -200,7 +212,7 @@ fn socket_stream_bridge(
         child_stdin_buf.with_read(|buf| child_stdin.write(buf))?;
         match socket_client.as_mut() {
             Some(client) => child_stdouterr_buf.with_read(|buf| client.write(buf))?,
-            None => child_stdouterr_buf.with_read(|buf| stdout.write(buf))?
+            None => child_stdouterr_buf.with_read(|buf| stdout.write(buf))?,
         };
     }
 
@@ -219,8 +231,8 @@ fn main() {
     let listener = UnixListener::bind(&sock_path)
         .unwrap_or_else(|err| die(format!("Could not create socket: {}", err)));
 
-    let (closer_read, mut closer_write) = pipe()
-        .unwrap_or_else(|err| die(format!("Could not create notification pipe: {}", err)));
+    let (closer_read, mut closer_write) =
+        pipe().unwrap_or_else(|err| die(format!("Could not create notification pipe: {}", err)));
 
     let mut child = Command::new(&command_line[0])
         .args(&command_line[1..])
@@ -233,25 +245,44 @@ fn main() {
     kill_child_on_signal(child.id() as i32)
         .unwrap_or_else(|err| die(format!("Could not bind signal handler: {}", err)));
 
-    let child_stdin = child.stdin.take().unwrap_or_else(|| die("No stdin pipe for child"));
-    let child_stdout = child.stdout.take().unwrap_or_else(|| die("No stdout pipe for child"));
-    let child_stderr = child.stderr.take().unwrap_or_else(|| die("No stderr pipe for child"));
+    let child_stdin = child
+        .stdin
+        .take()
+        .unwrap_or_else(|| die("No stdin pipe for child"));
+    let child_stdout = child
+        .stdout
+        .take()
+        .unwrap_or_else(|| die("No stdout pipe for child"));
+    let child_stderr = child
+        .stderr
+        .take()
+        .unwrap_or_else(|| die("No stderr pipe for child"));
 
     let worker = spawn(move || {
-        socket_stream_bridge(listener, child_stdin, child_stdout, child_stderr, closer_read)
+        socket_stream_bridge(
+            listener,
+            child_stdin,
+            child_stdout,
+            child_stderr,
+            closer_read,
+        )
     });
 
     let wait_result = child.wait();
     let to_close = [1u8; 1];
-    closer_write.write_all(&to_close)
-                .unwrap_or_else(|err| die(format!("Could not notify writer about child death: {}", err)));
+    closer_write.write_all(&to_close).unwrap_or_else(|err| {
+        die(format!(
+            "Could not notify writer about child death: {}",
+            err
+        ))
+    });
 
-    worker.join() // Two levels of failure: Result<Result<(), IOError>, ThreadPanic>
-          .unwrap_or_else(|err| die(format!("Worker thread panicked: {:?}", err)))
-          .unwrap_or_else(|err| die(format!("Worker thread died: {:?}", err)));
+    worker
+        .join() // Two levels of failure: Result<Result<(), IOError>, ThreadPanic>
+        .unwrap_or_else(|err| die(format!("Worker thread panicked: {:?}", err)))
+        .unwrap_or_else(|err| die(format!("Worker thread died: {:?}", err)));
 
-    remove_file(&sock_path)
-        .unwrap_or_else(|err| die(format!("Could not remove socket: {}", err)));
+    remove_file(&sock_path).unwrap_or_else(|err| die(format!("Could not remove socket: {}", err)));
 
     match wait_result {
         Ok(status) => exit(status.code().unwrap_or(254)),
--- src/signals.rs
+++ src/signals.rs
@@ -1,13 +1,20 @@
-use std::{io, mem::zeroed, ptr::null_mut};
+use std::io;
+use std::mem::zeroed;
+use std::ptr::null_mut;
 
+/// Stores the process ID of the child we spawned. This is in static state so
+/// the signal handler can use it to kill the process later.
 static mut CHILD_PID: i32 = 0;
 
+/// The C-level signal handler, actually kills the child process in repsonse to
+/// the signal.
 unsafe extern "C" fn on_terminate_signal() {
     // Can't meaningfully return anything here *or* do stdio to log the
     // error.
     libc::kill(CHILD_PID, libc::SIGTERM);
 }
 
+/// Safe wrapper around [`libc::sigaddset`].
 fn safe_sigaddset(sigset: *mut libc::sigset_t, signum: libc::c_int) -> io::Result<()> {
     if unsafe { libc::sigaddset(sigset, signum) } == 0 {
         Ok(())
@@ -16,6 +23,7 @@ fn safe_sigaddset(sigset: *mut libc::sigset_t, signum: libc::c_int) -> io::Resul
     }
 }
 
+/// Safe wrapper around [`libc::sigaction`].
 fn safe_sigaction(signum: libc::c_int, action: *const libc::sigaction) -> io::Result<()> {
     if unsafe { libc::sigaction(signum, action, null_mut()) } == 0 {
         Ok(())
@@ -24,6 +32,8 @@ fn safe_sigaction(signum: libc::c_int, action: *const libc::sigaction) -> io::Re
     }
 }
 
+/// Registers a signal handler that terminates the given process when *this*
+/// process receives a non-KILL termination signal.
 pub fn kill_child_on_signal(child_pid: i32) -> io::Result<()> {
     unsafe { CHILD_PID = child_pid };
 
